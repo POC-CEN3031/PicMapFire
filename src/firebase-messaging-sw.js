b4da74151502e2ee28c4060bb1d3ddee70405b31
Leo Diaz <LeonelD@ufl.edu>
2018-11-26 15:37:19 -0600

    Adding firebase functions

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -14,13 +14,14 @@
  * limitations under the License.
  */
 'use strict';
-
+ 
 import firebase from 'firebase/app';
 import 'firebase/messaging';
 import firebaseConfig from './firebase-config.json';
-
+ 
 // Configure Firebase.
 firebase.initializeApp(firebaseConfig.result);
-
+ 
 // Load Firebase messaging
 firebase.messaging();
+ 
