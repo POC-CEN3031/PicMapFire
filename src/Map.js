d3c0c29d05c1608b2e22202dc52ec48869b231c0
jiayangl <gscsdyang@gmail.com>
2018-12-02 21:17:53 -0500

    map.js update

--- src/Map.js
+++ src/Map.js
@@ -24,218 +24,89 @@ import { MaterialUtils } from './Utils';
  * Handles the Home and Feed UI.
  */
 export default class Map {
-  /**
-   * Initializes the Friendly Pix feeds.
-   * @constructor
-   */
-  constructor(firebaseHelper) {
-    this.firebaseHelper = firebaseHelper;
+    /**
+     * Initializes the Friendly Pix feeds.
+     * @constructor
+     */
+    constructor(firebaseHelper) {
+        this.firebaseHelper = firebaseHelper;
 
-    // List of all posts on the page.
-    this.posts = [];
-    // Map of posts that can be displayed.
-    this.newPosts = {};
+        // List of all posts on the page.
+        this.posts = [];
+        // Map of posts that can be displayed.
+        this.newPosts = {};
 
-    // Firebase SDK.
-    this.auth = firebase.auth();
+        // Firebase SDK.
+        this.auth = firebase.auth();
 
-    // // Pointers to DOM elements.
-    // this.pageFeed = $('#page-feed');
-    // this.feedImageContainer = $('.fp-image-container', this.pageFeed);
-    // this.noPostsMessage = $('.fp-no-posts', this.pageFeed);
-    // this.nextPageButton = $('.fp-next-page-button button');
-    // this.newPostsButton = $('.fp-new-posts-button button');
+        // // Pointers to DOM elements.
+        // this.pageFeed = $('#page-feed');
+        // this.feedImageContainer = $('.fp-image-container', this.pageFeed);
+        // this.noPostsMessage = $('.fp-no-posts', this.pageFeed);
+        // this.nextPageButton = $('.fp-next-page-button button');
+        // this.newPostsButton = $('.fp-new-posts-button button');
 
-    // // Event bindings.
-    // this.newPostsButton.click(() => this.showNewPosts());
-    initMap();
-    const googleMapsClient = require('@google/maps').createClient({
-      key: 'AIzaSyDX4CQXYTt_oUiaWlOhgwMZEpbMReiaa4M'
-    });
-  }
+        // // Event bindings.
+        // this.newPostsButton.click(() => this.showNewPosts());
+        initMap();
+        const googleMapsClient = require('@google/maps').createClient({
+            key: 'AIzaSyDX4CQXYTt_oUiaWlOhgwMZEpbMReiaa4M'
+        });
+    }
 
 
 
 };
 var map;
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 29.643439, lng:  -82.356024 },
-    zoom: 3,
-    style:[
-      {
-          "featureType": "all",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "weight": "2.00"
-              }
-          ]
-      },
-      {
-          "featureType": "all",
-          "elementType": "geometry.stroke",
-          "stylers": [
-              {
-                  "color": "#9c9c9c"
-              }
-          ]
-      },
-      {
-          "featureType": "all",
-          "elementType": "labels.text",
-          "stylers": [
-              {
-                  "visibility": "on"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#f2f2f2"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      },
-      {
-          "featureType": "landscape.man_made",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      },
-      {
-          "featureType": "poi",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "saturation": -100
-              },
-              {
-                  "lightness": 45
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#eeeeee"
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "color": "#7b7b7b"
-              }
-          ]
-      },
-      {
-          "featureType": "road",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      },
-      {
-          "featureType": "road.highway",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "simplified"
-              }
-          ]
-      },
-      {
-          "featureType": "road.arterial",
-          "elementType": "labels.icon",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "transit",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "visibility": "off"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "all",
-          "stylers": [
-              {
-                  "color": "#46bcec"
-              },
-              {
-                  "visibility": "on"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "geometry.fill",
-          "stylers": [
-              {
-                  "color": "#c8d7d4"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [
-              {
-                  "color": "#070707"
-              }
-          ]
-      },
-      {
-          "featureType": "water",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-              {
-                  "color": "#ffffff"
-              }
-          ]
-      }
-  ]
-  });
-  var UF = {lat: 29.643439, lng:  -82.356024};
-  var marker = new google.maps.Marker({
-    position: UF,
-    map: map
-  });
+    var UF = { lat: 29.643439, lng: -82.356024 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: UF,
+        zoom: 3,
+    });
+    
+    // var marker = new google.maps.Marker({
+    //     position: UF,
+    //     map: map
+    // });
+
+    // Create an array of alphabetical characters used to label the markers.
+    var labels = 'AB';
+
+var infoWin = new google.maps.InfoWindow();
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    var markers = locations.map(function (location, i) {
+        var marker = new google.maps.Marker({
+            position: location
+        });
+        google.maps.event.addListener(marker, 'click', function (evt) {
+            infoWin.setContent(location.info);
+            infoWin.open(map, marker);
+        })
+        return marker;
+    });
+
+    // Add a marker clusterer to manage the markers.
+    var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
+var a = {lat: 25.76, lng: -80.19, info: '<a href="https://pocs-92a3c.firebaseapp.com/post/-LSlne6dNqKLm5y6oUO3">View Post</a>' };
+var b = {lat: 40.70, lng: -74.00, info:"https://pocs-92a3c.firebaseapp.com/post/-LSlne6dNqKLm5y6oUO3" };
+// var c = {lat: 34.05, lng: 118.2, info: };
+// var d = {lat: 42.36, lng: 71.06, info: };
+// var e = {lat: 29.76, lng: 95.37, info: };
+// var f = {lat: 47.61, lng: 122.3, info: };
+// var g = {lat: 33.75, lng: 84.39, info: };
+// var h = {lat: 41.88, lng: 87.63, info: };
+// var i = {lat: 28.54, lng: 81.38, info: };
+// var j = {lat: 32.77, lng: 96.79, info: };
+
+var locations = [
+    a,b
+    //,c,d,e,f,g,h,i,j
+]
+
